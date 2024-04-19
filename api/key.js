e43428b3b8d88aae2877d835b695b4e181f99ca3
Sumanth-dara <164885598+Sumanth-dara@users.noreply.github.com>
2024-04-19 09:20:31 -0500

    Add files via upload

---
+++ api/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://darasumanth6363:Sumanth123@cluster0.ln3nhu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
